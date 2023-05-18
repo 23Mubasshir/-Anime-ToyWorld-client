3e33682c9b86d7a3f88068a3c3b047d246c304fe
23Mubasshir <23mubasshir@gmail.com>
2023-05-18 19:23:00 +0600

    Login Bug

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -4,16 +4,14 @@ import { FaGoogle } from "react-icons/fa";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
 
-
 const SignIn = () => {
-  const { signIn, signInWithGoogle } =
-  useContext(AuthContext);
+  const { signIn, signInWithGoogle } = useContext(AuthContext);
 
-// -----Success and error message-----
+  // -----Success and error message-----
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
 
-// -----Regular Sign In-----
+  // -----Regular Sign In-----
   const handleSignIn = (event) => {
     event.preventDefault();
     const form = event.target;
@@ -28,7 +26,7 @@ const SignIn = () => {
         const loggedUser = result.user;
         // console.log(loggedUser);
         setSuccess("User has been signed in Successfully");
-        navigate(from, { replace: true });
+        // navigate(from, { replace: true });
       })
       .catch((error) => {
         console.log(error);
@@ -36,14 +34,13 @@ const SignIn = () => {
       });
   };
 
-
   //  < ----- Google Sign-in ----->
   const handleGoogleSignIn = () => {
     signInWithGoogle()
       .then((result) => {
         // Signed in
         const loggedUser = result.user;
-        navigate(from, { replace: true });
+        // navigate(from, { replace: true });
         // console.log(loggedUser);
         setError("");
         setSuccess("User has been Signed In Successfully");
@@ -89,15 +86,19 @@ const SignIn = () => {
                   />
                   <label className="label">
                     <p>
-                     Don't have an account ?{" "}
+                      Don't have an account ?{" "}
                       <Link className="text-orange-500" to="/sign-up">
                         Sign Up
                       </Link>
                     </p>
                   </label>
                   {/* <-----Error and Success message-----> */}
-                  <p className=" text-green-400 font-bold
-                  " >{success}</p>
+                  <p
+                    className=" text-green-400 font-bold
+                  "
+                  >
+                    {success}
+                  </p>
                   <p className="text-red-400  font-bold">{error}</p>
                 </div>
                 <div className="form-control mt-6">
@@ -112,7 +113,10 @@ const SignIn = () => {
               <div className="">
                 <h1 className="text-center pt-3">OR</h1>
                 <h1 className="pb-3 text-neutral-500">Sign In with </h1>
-                <button onClick={handleGoogleSignIn} className="btn btn-primary btn-block">
+                <button
+                  onClick={handleGoogleSignIn}
+                  className="btn btn-primary btn-block"
+                >
                   <FaGoogle />
                   <span className="mx-2"> Google</span>
                 </button>
