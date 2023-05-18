489f5e56ecdf0d1dbf85a81cf20c9bcb5bbfd614
23Mubasshir <23mubasshir@gmail.com>
2023-05-18 09:10:42 +0600

    Login Page added

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
-    return (
-        <div>
-            <h1>Sign in</h1>
+
+    const handleSignIn = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log( email, password);
+
+        // signIn(email, password)
+        // .then (result => {
+        //   const user = result.user;
+        //   console.log(user)
+        // })
+        // .catch(error => console.log(error))
+    }
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <form onClick={handleSignIn}>
+              <h1 className="text-5xl font-bold">Login</h1>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="Your Email"
+                  name="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  name="password"
+                  type="password"
+                  placeholder="Your Password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <p>
+                    Don't have an account?{" "}
+                    <Link
+                      to="/sign-up"
+                      className="text-orange-500 font font-bold underline"
+                    >
+                      Sign Up
+                    </Link>
+                  </p>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input className="btn btn-primary" type="submit" value="Sign in" />
+              </div>
+              </form>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
