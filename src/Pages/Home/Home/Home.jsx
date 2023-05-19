e5c08a627f4fbb40349a65ae2f715528633381ee
23Mubasshir <23mubasshir@gmail.com>
2023-05-19 12:18:24 +0600

    Banner image Added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import WhyBest from '../WhyBest/WhyBest';
+import Review from '../Review/Review';
 
 const Home = () => {
     return (
         <div>
-            
+            <Banner></Banner>
+            <WhyBest></WhyBest>
+            <Review></Review>
         </div>
     );
 };
---
+++ src/Pages/Home/WhyBest/WhyBest.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WhyBest = () => {
+    return (
+        <div>
+            <h1>Why BEST section</h1>
+        </div>
+    );
+};
+
+export default WhyBest;
